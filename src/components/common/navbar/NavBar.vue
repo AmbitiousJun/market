32289934c02cfe852e18b1c6a1915c72e00f96cc
Jun Ambitious <1535820771@qq.com>
2021-08-06 16:12:55 +0800

    添加一个导航栏组件

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
+
+<style scoped>
+  .nav-bar {
+    display: flex;
+    line-height: 6.6vh;
+    height: 6.6vh;
+    box-shadow: 0 1px 8px rgba(100, 100, 100, 0.2);
+  }
+
+  .left,
+  .right {
+    width: 16vw;
+  }
+
+  .center {
+    flex: 1;
+    text-align: center;
+  }
+</style>
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,9 +1,24 @@
 <template>
-  <h2>首页</h2>
+  <!-- 顶部导航栏 开始 -->
+  <nav-bar class="home-nav">
+    <div slot="center">购物商城</div>
+  </nav-bar>
+  <!-- 顶部导航栏 结束 -->
 </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+  import NavBar from 'components/common/navbar/NavBar'
+  export default {
+    name: 'Home',
+    components: {
+      NavBar
+    }
+  }
 </script>
+
+<style scoped>
+  .home-nav {
+    background-color: var(--color-tint);
+    color: white;
+  }
+</style>
